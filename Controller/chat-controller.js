41a59d56e6a531e0c04fbb7166e10b53d5a29289
Kirti <kirtijhala1110@gmail.com>
2025-05-20 00:10:29 +0530

    one to one chat functionality added

--- Controller/chat-controller.js
+++ Controller/chat-controller.js
@@ -15,7 +15,9 @@ export const sendMessage = async (req, res, next) => {
         console.log("status...");
         if (send) {
             console.log('msg send successfully : ' + send);
-            return res.status(201).json({ msg: `${message} send successfully` });
+            const user = await User.findById({ receiverId });
+            user.notification.push(send);
+            return res.status(201).json({ msg: `${message} send successfully`});
         } else {
             console.log("sending failed!");
             return res.status(401).json({ error: 'sending failed dude!' })
@@ -26,25 +28,54 @@ export const sendMessage = async (req, res, next) => {
     }
 }
 
-export const viewChat = async (req, res, next) => {
-    const { id } = req.params;
-    console.log("id : " + id);
+// export const sendMessage = async (req, res, next) => {
+//     const { message, receiverId } = req.body;
+//     const { id } = req.params;
+//     try {
+//         console.log("sender's id : " + id + " recever's id : " + receiverId);
+//         console.log(" msg : " + message);
+//         const send = await Chat.create({
+//             senderId: id,
+//             receiverId: receiverId,
+//             message: message
+//         });
+//         console.log("status...");
+//         if (send) {
+//             const user = await User.findById(receiverId);
+//             if (user) {
+//                 user.notification.push(send);
+//                 console.log("Notification in user schema...")
+//             }
+//             console.log('msg send successfully : ' + send);
+//             return res.status(201).json({ msg: `${message} send successfully` });
+//         } else {
+//             console.log("sending failed!");
+//             return res.status(401).json({ error: 'sending failed dude!' })
+//         }
+//     } catch (error) {
+//         console.log("error : " + error);
+//         return res.status(501).json({ error: 'Internal Server Error', error })
+//     }
+// }
+
+//===================================================================
+
+export const viewChat = async (req, res) => {
     try {
-        console.log("Viewing the chat...")
-        const msgss = await Chat.find({ $or: [{ senderId: id }, { receiverId: id }] })
-            .populate('senderId', 'name').populate('receiverId', 'name');
-        if (msgss) {
-            console.log('msg get: ' + msgss);
-            return res.status(201).json({ messages: msgss });
-        } else {
-            console.log("Sorry, Can not fetch the data");
-            return res.status(401).json({ error: 'sending failed dude!' })
-        }
+        const { id } = req.params;
+
+        const messages = await Chat.find({
+            $or: [{ senderId: id }, { receiverId: id }]
+        }).sort({ createdAt: 1 });
+
+        res.status(200).json({ messages });
+
     } catch (error) {
-        console.log("error : " + error);
-        return res.status(501).json({ error: 'Internal Server Error', error })
+        console.error("Error fetching messages:", error);
+        res.status(500).json({ error: "Server error" });
     }
-}
+
+};
 
 // export const sendMessage = async (req, res, next) => {
 //     const { message, receiverId } = req.body;
